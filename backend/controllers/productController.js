244a7d90378643a17304db0cf35e121984620537
Shruti <jaiswalshruti1102@gmail.com>
2022-03-01 02:08:17 +0530

    All routes created

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,76 @@
 const Product = require("../models/productModel")
 
 
-exports.getAllProducts = (req,res)=>{
-    res.status(200).json({message:"workingg"})
+//Create Product --Admin
+exports.createProduct = async (req,res,next)=>{
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        sucess:true,
+        product
+    })
 }
+
+//get all product
+exports.getAllProducts = async (req,res)=>{
+    
+    const products = await Product.find();
+    res.status(200).json({
+        sucess:true,
+        products
+    })
+}
+//get single product
+
+exports.getProductDetails = async (req,res)=>{
+    
+    const product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            sucess:false,
+            message:"Product not found"
+        })
+    }
+    res.status(200).json({
+        sucess:true,
+        product
+    })
+}
+
+
+//update product --admin
+exports.updateProduct = async (req,res,next)=>{
+    let product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            sucess:false,
+            message:"Product not found"
+        })
+    }
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false
+    });
+    res.status(200).json({
+        sucess:true,
+        product
+    })
+}
+
+//delete product --admin
+exports.deleteProduct = async (req,res,next)=>{
+    let product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            sucess:false,
+            message:"Product not found"
+        })
+    }
+   await product.remove();
+
+    res.status(200).json({
+        sucess:true,
+        message:"Product Deleted Successfully"
+    })
+}
+
