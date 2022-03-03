a8f45fdf01a5b6b64b62c9a684e659a81642475d
Shruti <jaiswalshruti1102@gmail.com>
2022-03-03 19:16:39 +0530

    file changed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel")
 
-
+//
 //Register a User
 exports.registerUser = catchAsyncErrors(async(req,res,next) => {
     const { name, email, password } = req.body;
